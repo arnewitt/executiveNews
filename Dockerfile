8c89a69ba1928222c60c7ed9bdbf1c1458c1977d
Arne Wittmaack <arne.wittmaack@web.de>
2024-06-04 23:50:36 +0200

    Make execution time env variable

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,12 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+# Use OpenAI compatible API endpoint from NVIDIA API catalog
 ENV API_KEY=api_key
 ENV BASE_URL=base_url
+
+# Define the specific time to run (24-hour format)
+ENV TARGET_HOUR=6
+ENV TARGET_MINUTE=30
 
 CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 
+import os
+
 from utils.utils import check_time_and_run
 
 if __name__ == "__main__":
     # Define the specific time to run (24-hour format)
-    target_hour = 7
-    target_minute = 30
+    target_hour = os.environ.get("TARGET_HOUR", "7")
+    target_minute = os.environ.get("TARGET_MINUTE", "30")
 
     check_time_and_run(target_hour, target_minute)
